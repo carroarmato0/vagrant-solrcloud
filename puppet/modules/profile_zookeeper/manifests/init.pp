e626a9284261bf1628379ec95cf0491b10f60da9
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-06-14 10:54:08 +0200

    Allow changing logging level for zookeeper
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- puppet/modules/profile_zookeeper/manifests/init.pp
+++ puppet/modules/profile_zookeeper/manifests/init.pp
@@ -2,11 +2,14 @@
 class profile_zookeeper (
   $id,
   $servers,
-  $tick_time      = 2000,
-  $init_limit     = 10,
-  $sync_limit     = 5,
-  $install_method = 'archive',
-  $manage_java    = true,
+  $tick_time             = 2000,
+  $init_limit            = 10,
+  $sync_limit            = 5,
+  $install_method        = 'archive',
+  $manage_java           = true,
+  $console_threshold     = 'INFO',
+  $rollingfile_threshold = 'INFO',
+  $tracefile_threshold   = 'TRACE',
 ) {
 
   if $manage_java {
@@ -14,15 +17,18 @@ class profile_zookeeper (
   }
 
   class {'zookeeper':
-    id                  => $id,
-    servers             => $servers,
-    tick_time           => $tick_time,
-    init_limit          => $init_limit,
-    sync_limit          => $sync_limit,
-    install_method      => $install_method,
-    install_java        => false,
-    service_provider    => 'systemd',
-    manage_service_file => true,
+    id                    => $id,
+    servers               => $servers,
+    tick_time             => $tick_time,
+    init_limit            => $init_limit,
+    sync_limit            => $sync_limit,
+    install_method        => $install_method,
+    install_java          => false,
+    service_provider      => 'systemd',
+    manage_service_file   => true,
+    console_threshold     => $console_threshold,
+    rollingfile_threshold => $rollingfile_threshold,
+    tracefile_threshold   => $tracefile_threshold,
   }
 
 }
