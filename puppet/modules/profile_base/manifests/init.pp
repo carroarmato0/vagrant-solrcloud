162c671a5fea8413ceb152be00e70a7292472a1a
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2017-06-14 13:31:57 +0200

    Add /etc/hosts entries in there
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- puppet/modules/profile_base/manifests/init.pp
+++ puppet/modules/profile_base/manifests/init.pp
@@ -4,6 +4,18 @@ class profile_base {
   require profile_base::repo
   require profile_base::packages
 
+  file {'/etc/hosts':
+    ensure  => file,
+    content => "
+      127.0.0.1 localhost
+      ::1 localhost
+
+      192.168.10.11 zoo01
+      192.168.10.21 solr01
+      192.168.10.22 solr02
+    ",
+  }
+
   Class['profile_base::repo']->Class['profile_base::packages']
 
 }
